8566cafd9c6efa488dedf17478ac19a981320961
GAKUSEISENSEI <98035131+GAKUSEISENSEI@users.noreply.github.com>
2023-01-05 01:45:07 +0400

    Update ShmyakToken.sol

--- src/ShmyakToken.sol
+++ src/ShmyakToken.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-
+import "./Exchange.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract ShmyakToken is ERC20Capped, ERC20Burnable {
-    address payable public owner;
+contract ShmyakToken is ERC20, ERC20Burnable {
+    address  public owner;
     uint public blockReward;
-    constructor(uint256 cap, uint reward) ERC20("ShmyakToken", "SHMK") ERC20Capped(cap * (10 ** 18)){
-        owner = payable(msg.sender);
-        _mint(owner, 7000000 * (10 ** 18));
-        blockReward = reward * (10 ** 18);
+    uint public _cap;
+    Exchange public exchange;
+    constructor() ERC20("ShmyakToken", "SHMK"){
+        owner = msg.sender;
+        _cap = 1000000 * (10 ** 18);
+        blockReward = (10 ** 18);
 
     }
 
-    function _mint(address account, uint256 amount) internal virtual override (ERC20Capped, ERC20) {
-        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+    function setExcAddress(Exchange _exchange) public onlyOwner{
+        exchange = _exchange;
+    }
+
+    function _owner() public view returns (address){
+        return owner;
+    }
+
+    function mint (address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function cap() public view returns(uint){
+        return _cap;
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override (ERC20) {
+        require(ERC20.totalSupply() + amount <= _cap, "cap exceeded");
         super._mint(account, amount);
     }
 
@@ -34,13 +51,13 @@ contract ShmyakToken is ERC20Capped, ERC20Burnable {
         blockReward = reward * (10 ** 18);
     }
     modifier onlyOwner {
-        require(msg.sender == owner, "You`re not an owner!");
+        require(msg.sender == owner || msg.sender == address(exchange), "You`re not an owner!");
         _;
         
     }
 
     function destroy() public onlyOwner{
-        selfdestruct(owner);
+        selfdestruct(payable(owner));
     }
 }
 
